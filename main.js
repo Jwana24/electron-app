8f489e65f6b821666f38149aae74394f5d6bbd21
Johanna DETRIEUX <johanna.detrieux@gmail.com>
2024-08-07 16:08:56 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('node:path');
+
+const createWindow = () => {
+  const windown = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  windown.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong');
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
