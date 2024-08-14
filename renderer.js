10f3e06e2c263af51d1ad5948945fdee5042e8cc
Johanna DETRIEUX <johanna.detrieux@gmail.com>
2024-08-14 17:16:45 +0200

    Adding ViteJS, TypeScript and ReactJS to the app

--- renderer.js
+++
@@ -1,9 +0,0 @@
-const information = document.getElementById('info');
-information.innerText = `Cette application utilise Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), et Electron (v${versions.electron()})`;
-
-const func = async () => {
-  const response = await versions.ping()
-  console.log(response) // Affichera 'pong'
-}
-
-func()
